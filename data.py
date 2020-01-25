e1fcfc5760913a6d2f2def61080edbd070fc5f6e
Max Bluestone <mbluestone93@gmail.com>
2020-01-24 19:21:20 -0500

    big update, working model for demo 2

--- data.py
+++ data.py
@@ -1,40 +1,59 @@
 from pysmiles import read_smiles
 import networkx as nx
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
+import sys
+import os
 
-from torch-geometric.data import Data, Dataset
+from skmultilearn.model_selection import iterative_train_test_split
+
+from torch_geometric.data import Data, Dataset, DataLoader
+
+def load_data(path):
+    '''
+    Custom data loading function, depends on format of saved data
+    '''
+    
+    data = pd.read_csv(path)
+    X = np.matrix(data.smiles.values).T
+    y = np.matrix(data.iloc[:,1:].values)
+    
+    return X, y
+
+def split_data(X, y, test_size = 0.2, save_path = None):
+    
+    X_train, y_train, X_test, y_test = iterative_train_test_split(X,y,test_size = 0.2)
+    
+    train_data = pd.concat(X_train,y_train)
+    val_data = pd.concat(X_train,y_train)
+    test_data = pd.concat(X_train,y_train)
 
 class Molecule(Data):
     '''
     Molecule Data Class
     Subclass of torch-geometric.data.Data
     '''
-    def __init__(self, smiles_string: str):
-        
-        # smiles string
-        self.smiles_string = smiles_string
+    def __init__(self, smiles_string: str, y_list: list):
 
-        # create graph from smiles
-        self.graph = read_smiles(self.smiles_string)
+        # create graph from smiles 
+        # (the sys code is to block a pysmiles warning about iseometric stuff)
+        sys.stdout = open(os.devnull, 'w')
+        self.graph = read_smiles(smiles_string)
+        sys.stdout = sys.__stdout__
         
-        # create adjacency matrix from graph
-        self.adjacency_matrix = nx.to_numpy_matrix(self.graph, weight='order')
+        # inherit superclass from torch-geometric
+        super().__init__(x = torch.tensor(self.extract_features(), 
+                                          dtype=torch.float),
+                         edge_index = torch.tensor(self.graph_to_edge_index(), 
+                                                   dtype=torch.long),
+                         y = torch.tensor(y_list, dtype=torch.float32))
         
-        # load atom dict for converting atomic symbol to atomic number
-        self.atom_dict = pd.read_csv('raw_data/atom_info.txt',sep=',').set_index('Symbol')['AtomicNumber'].to_dict()
-        
-        super().__init__(x = torch.tensor(self.extract_features()),
-                         edge_index = self.graph)
-        
-    def __str__(self):
-        print('SMILES:',self.s)
-        print('Molecule structure:\n',nx.draw(self.graph))
+        # remove graph attribute, necessary to inhereit from superclass
+        del self.graph
     
-    def __len__(self):
-        return len(self.graph.nodes)
+    #def __len__(self):
+     #   return len(self.graph.nodes)
     
     def extract_features(self):
         
@@ -57,8 +76,9 @@ class Molecule(Data):
     
     def convert_atom_to_vector(self,atom):
         
+        atom_dict = pd.read_csv('raw_data/atom_info.txt',sep=',').set_index('Symbol')['AtomicNumber'].to_dict()
         feature_vector = np.array([])
-        feature_vector = np.append(feature_vector,int(self.atom_dict[atom['element']]))
+        feature_vector = np.append(feature_vector,int(atom_dict[atom['element']]))
         feature_vector = np.append(feature_vector,atom['charge'])
         if atom['aromatic']:
             feature_vector = np.append(feature_vector,1)
@@ -67,11 +87,31 @@ class Molecule(Data):
         feature_vector = np.append(feature_vector,atom['hcount'])
         
         return feature_vector
+    
+    def graph_to_edge_index(self):
+        '''
+        Convert the adjacency matrix to an edge index for representing
+        graph connectivity in COO format
+        '''
+        adjacency_matrix = nx.to_numpy_matrix(self.graph, weight='order')
+        #print(adjacency_matrix)
+        edge_index = np.array([])
+        for i in range(len(self.graph.nodes)):
+            for j in range(len(self.graph.nodes)):
+                if adjacency_matrix[i,j] != 0:
+                    if edge_index.size == 0:
+                        edge_index = np.array([i,j])
+                    else:
+                        edge_index = np.vstack((edge_index, np.array([i,j])))
+                        
+        return edge_index.T
+                    
+            
         
-class MoleculeDataset(Dataset):
+class MoleculeDataset():
     """Molecules dataset."""
 
-    def __init__(self, smiles_list, transform=None):
+    def __init__(self, X, y, transform=None):
         """
         Args:
             smiles_list (string): Path to the csv file with annotations.
@@ -79,32 +119,39 @@ class MoleculeDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.smiles_list = smiles_list
+        self.X = X
+        self.y = y
         self.transform = transform
 
     def __len__(self):
-        return len(self.smiles_list)
+        return self.data.shape[0]
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
             
-        molecule = Molecule(self.smiles_list[idx])
-        adjacency = molecule.adjacency_matrix
-        features = molecule.extract_features()
-        sample = {'adjacency': adjacency, 'features': features}
+        molecule = Molecule(self.X[idx],
+                           self.y[idx])
 
         if self.transform:
-            sample = self.transform(sample)
+            molecule = self.transform(molecule)
 
-        return sample
+        return molecule
     
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
+    def _download(self):
+        pass
 
-    def __call__(self, sample):
-        
-        adjacency, features = sample['adjacency'], sample['features']
+    def _process(self):
+        pass
+    
+    def get(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            
+        molecule = Molecule(self.X[idx],
+                           self.y[idx])
 
-        return {'adjacency': torch.from_numpy(adjacency),
-                'features': torch.from_numpy(features)}
+        if self.transform:
+            molecule = self.transform(molecule)
+
+        return molecule
--- train.py
+++ train.py
@@ -1,20 +1,199 @@
-import torch.nn
+import pandas as pd
+import torch
+from torch.nn import Linear, BCEWithLogitsLoss
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch.optim import Adam
+from torch_geometric.data import DataLoader
+from torch_geometric.nn import GCNConv, global_add_pool
 from torch_geometric.transforms import AddSelfLoops, ToDense
+from data import MoleculeDataset
+import random
 
-class Net(nn.Module):
+#### DATA #####
+#load
+data_path = '~/github/MolNet/raw_data/sider_data/sider.csv'
+sider_data = pd.read_csv(data_path)
+smiles_list = sider_data.smiles.to_list()
+dataset = MoleculeDataset(data_path, transform=AddSelfLoops())
+
+# temporary
+for i in range(sider_data.shape[0]):
+    dataset[i].y = torch.tensor([sider_data.iloc[i,1:].to_list()],
+                                 dtype=torch.float32)
+dataloader = DataLoader(dataset,batch_size=5,shuffle=True)
+
+train_path = '~/github/MolNet/raw_data/sider_data/train_sider.csv'
+val_path = '~/github/MolNet/raw_data/sider_data/val_sider.csv'
+test_path = '~/github/MolNet/raw_data/sider_data/test_sider.csv'
+
+train_data = train_data.reset_index().drop('index',axis=1)
+val_data = val_data.reset_index().drop('index',axis=1)
+test_data = test_data.reset_index().drop('index',axis=1)
+
+train_data.to_csv(train_path)
+val_data.to_csv(val_path)
+test_data.to_csv(test_path)
+
+train_dataset = MoleculeDataset(train_path, transform=AddSelfLoops())
+val_dataset = MoleculeDataset(val_path, transform=AddSelfLoops())
+test_dataset = MoleculeDataset(test_path, transform=AddSelfLoops())
+
+train_dataloader = DataLoader(train_dataset,batch_size=50,shuffle=True)
+val_dataloader = DataLoader(val_dataset,batch_size=50,shuffle=True)
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+# intialize data loaders
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+
+#### MODEL CLASS #####
+class MolNet(torch.nn.Module):
     def __init__(self, num_node_features: int, num_classes: int):
         super(Net, self).__init__()
         self.conv1 = GCNConv(num_node_features, 16)
-        self.conv2 = GCNConv(16, dataset.num_classes)
+        self.conv2 = GCNConv(16, 16)
+        self.lin1 = Linear(16,100)
+        self.lin2 = Linear(100,num_classes)
+        self.sig = Sigmoid()
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        x, edge_index, batch_vec = data.x, data.edge_index, data.batch
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        sum_vector = global_add_pool(x,batch = batch_vec)
+        x = F.relu(sum_vector)
+        x = F.dropout(x, training=self.training)
+        x = self.lin1(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.lin2(x)
 
-        return F.log_softmax(x, dim=1)
+        return x
+
+train_losses = []
+train_accuracies = []
+val_losses = []
+val_accuracies = []
+
+def create_model(num_layers: int, num_classes: int,
+                 pretrain: bool) -> torchvision.models.resnet.ResNet:
+    """
+    Instantiate the ResNet model.
+    Args:
+        num_layers: Number of layers to use in the ResNet model from [18, 34, 50, 101, 152].
+        num_classes: Number of classes in the dataset.
+        pretrain: Use pretrained ResNet weights.
+    Returns:
+        The instantiated ResNet model with the requested parameters.
+    """
+    assert num_layers in (
+        18, 34, 50, 101, 152
+    ), f"Invalid number of ResNet Layers. Must be one of [18, 34, 50, 101, 152] and not {num_layers}"
+    model_constructor = getattr(torchvision.models, f"resnet{num_layers}")
+    model = model_constructor(num_classes=num_classes)
+
+    if pretrain:
+        pretrained = model_constructor(pretrained=True).state_dict()
+        if num_classes != pretrained["fc.weight"].size(0):
+            del pretrained["fc.weight"], pretrained["fc.bias"]
+        model.load_state_dict(state_dict=pretrained, strict=False)
+    return model
+
+def get_pos_weights(labels):
+    '''
+    Calculate the positive weights for each class
+    '''
+    weights = [(labels.shape[0]-labels[:,i].sum())/labels[:,i].sum() 
+               for i in range(labels.shape[1])]
+    return torch.tensor(weights)
+
+def train_model():
+    '''
+    Function for training model
+    
+    Args:
+        num_epochs
+    '''
+    
+    # use gpu if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    num_node_features = 4
+    num_classes = train_dataset[0].y.size(1)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = Net(num_node_features, num_classes).to(device)
+    optimizer = Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+    criterion = BCEWithLogitsLoss()
+    
+    pos_weights = get_pos_weights()
+    
+
+def train_helper(model, num_epochs, dataloaders, pos_weights):
+    '''
+    Helper function for training model
+    
+    Args:
+        model:
+        num_epochs
+    '''
+
+    for epoch in range(num_epochs):
+
+        print(f'Epoch {epoch}:')
+        model.train()
+
+        train_running_loss = 0.0
+        train_running_accuracy = 0.0
+
+        for inputs in train_dataloader:
+
+            optimizer.zero_grad()
+            with torch.set_grad_enabled(mode=True):
+                out = model(inputs)
+                train_loss = criterion(out, inputs.y)
+                train_acc = ((torch.sigmoid(out)>0.5).numpy()==inputs.y.numpy()).mean()
+                train_loss.backward()
+                optimizer.step()
+
+            train_running_loss += train_loss.item() * inputs.y.size(0)
+            train_running_accuracy += train_acc * inputs.y.size(0)
+
+        epoch_train_loss = train_running_loss/len(train_dataset)
+        train_losses.append(epoch_train_loss)
+        epoch_train_acc = train_running_accuracy/len(train_dataset)
+        train_accuracies.append(epoch_train_acc)
+
+        print(f'Train:\nLoss = {epoch_train_loss}\nAcc = {epoch_train_acc}')   
+
+        model.eval()
+
+        val_running_loss = 0.0
+        val_running_accuracy = 0.0
+
+        for inputs in val_dataloader:
+
+            with torch.set_grad_enabled(mode=False):
+                out = model(inputs)
+                val_loss = criterion(out, inputs.y)
+                val_acc = ((torch.sigmoid(out)>0.5).numpy()==inputs.y.numpy()).mean()
+
+            val_running_loss += val_loss.item() * inputs.y.size(0)
+            val_running_accuracy += val_acc * inputs.y.size(0)
+
+        epoch_val_loss = val_running_loss/len(val_dataset)
+        val_losses.append(epoch_val_loss)
+        epoch_val_acc = val_running_accuracy/len(val_dataset)
+        val_accuracies.append(epoch_val_acc) 
+
+        print(f'Validation:\nLoss = {epoch_val_loss}\nAcc = {val_accuracies}')
