aff4891cc682db2d46414a333dd5be02dedc1a99
Max Bluestone <mbluestone93@gmail.com>
2020-01-28 13:57:11 -0500

    script for training the model

---
+++ train_model.py
@@ -0,0 +1,14 @@
+import config
+
+train_model(data_dir=config.data_dir,
+            model_type=config.model_type,
+            num_epochs=config.num_epochs,
+            num_graph_layers=config.num_graph_layers,
+            num_linear_layers=config.num_linear_layers,
+            learning_rate=config.learning_rate,
+            learning_rate_decay=config.learning_rate_decay,
+            weight_decay=config.weight_decay, 
+            batch_size=config.batch_size,
+            log_csv=config.log_csv,
+            log_file=config.log_file,
+            pretrain_load_path=config.pretrain_load_path)
